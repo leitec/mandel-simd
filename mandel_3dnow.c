5350f3554208512352d095824306ed223200d3c1
Claudio Leite <leitec@staticky.com>
2015-07-15 22:11:21 -0400

    experimental changes

--- mandel_3dnow.c
+++ mandel_3dnow.c
@@ -9,7 +9,7 @@ mandel_3dnow(unsigned char *image, const struct spec *s)
 {
     v2sf xmin, ymin, xscale, yscale, iter_scale, depth_scale;
     v2sf threshold = (v2sf) { 4.0, 4.0 };
-    v2sf one = (v2sf) { 1.0, 1.0 };
+    v2si one = (v2si) { 1, 1 };
 
     xmin = (v2sf) { s->xlim[0], s->xlim[0] };
     ymin = (v2sf) { s->ylim[0], s->ylim[0] };
@@ -26,7 +26,7 @@ mandel_3dnow(unsigned char *image, const struct spec *s)
     #pragma omp parallel for schedule(dynamic, 1)
     for (int y = 0; y < s->height; y++) {
         for (int x = 0; x < s->width; x += 2) {
-            v2sf mx = (v2sf) { x + 1, x };
+            v2sf mx = (v2sf) { x, x+1 };
             v2sf my = (v2sf) { y, y };
             v2sf cr = __builtin_ia32_pfadd(__builtin_ia32_pfmul(mx, xscale), xmin);
             v2sf ci = __builtin_ia32_pfadd(__builtin_ia32_pfmul(my, yscale), ymin);
@@ -34,7 +34,7 @@ mandel_3dnow(unsigned char *image, const struct spec *s)
             v2sf zi = ci;
 
             int k = 1;
-            v2sf mk = { 0, 0 };
+            v2si mk = (v2si) { 1, 1 };
             while (++k < s->iterations) {
                 /* Compute z1 from z0 */
                 v2sf zr2 = __builtin_ia32_pfmul(zr, zr);
@@ -51,20 +51,24 @@ mandel_3dnow(unsigned char *image, const struct spec *s)
                 zi2 = __builtin_ia32_pfmul(zi, zi);
                 v2sf mag2 = __builtin_ia32_pfadd(zr2, zi2);
                 v2si mask = __builtin_ia32_pfcmpge(mag2, threshold);
-                mk = __builtin_ia32_pfadd(mk, one);
+		mk = __builtin_ia32_paddd(mk, one);
+                mk = __builtin_ia32_psubd(mk, __builtin_ia32_pand(one, mask));
 
                 /* Early bailout? */
                 int imask[2];
-
                 *(v2si *)&imask = mask;
+
                 if (imask[0] || imask[1])
                     break;
             }
-            mk = __builtin_ia32_pfmul(mk, iter_scale);
-            mk = __builtin_ia32_pfmul(__builtin_ia32_pfrsqrt(mk), mk);
-            mk = __builtin_ia32_pfmul(mk, depth_scale);
 
-            v2si pixels = __builtin_ia32_pf2id(mk);
+            v2sf fmk = __builtin_ia32_pi2fd(mk);
+
+            fmk = __builtin_ia32_pfmul(fmk, iter_scale);
+            fmk = __builtin_ia32_pfmul(__builtin_ia32_pfrsqrt(fmk), fmk);
+            fmk = __builtin_ia32_pfmul(fmk, depth_scale);
+
+            v2si pixels = __builtin_ia32_pf2id(fmk);
 
             unsigned char *dst = image + y * s->width * 3 + x * 3;
             unsigned char *src = (unsigned char *)&pixels;
